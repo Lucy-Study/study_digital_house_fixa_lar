c02086dfac5cebe6a40a301297f071dd854786b4
Dorivan Grangeiro Almeida <dorivan.almeida@gmail.com>
2020-05-22 12:04:06 -0400

    adicionando models de Zona

---
+++ models/Zona.js
@@ -0,0 +1,34 @@
+module.exports = (sequelize, DataType) => {
+    const Zona = sequelize.define(
+      "Zona",
+      {
+        id: {
+          type: DataType.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        }, 
+        nome: DataType.STRING(30),
+      },
+
+      {
+        tableName: "zonas",
+        timestamps: false,
+      }
+    );
+  
+    Zona.associate = (pastaModels) => {
+      Zona.hasMany(pastaModels.Prestador, {
+        foreignKey: "zonas_id"
+      });
+
+      //RELACIONAMENTO DE ZONA COM PRESTADOR M x N.
+      Zona.belongsToMany(pastaModels.Prestador,{
+        through: 'PrestadorZona',
+        foreignKey: 'zonas_id',
+      });
+    };
+  
+
+    return Zona;
+  };
+  
