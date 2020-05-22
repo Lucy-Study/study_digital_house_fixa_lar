9212059d35b70214dded653019bd5113dd09ae7c
Dorivan Grangeiro Almeida <dorivan.almeida@gmail.com>
2020-05-22 12:03:48 -0400

    adicionando models de Prestador

---
+++ models/Prestador.js
@@ -0,0 +1,60 @@
+module.exports = (sequelize, DataType) => {
+    const Prestador = sequelize.define(
+      "Prestador",
+      {
+        id: {
+          type: DataType.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        cpf: {
+            type:DataType.INTEGER,
+            unique: true
+        },
+        nome: DataType.STRING(200),
+        bio:{
+            type:DataType.STRING,
+            allowNull: true,
+        },
+        genero: DataType.STRING,
+        email: DataType.STRING(200),
+        logradouro: DataType.STRING(300),
+        cep: DataType.STRING(15),
+        complemento:{
+            type:DataType.STRING(300),
+            allowNull: true,
+        },
+        senha: DataType.STRING(300),
+        zonas_id:DataType.INTEGER,
+        cidades_id:DataType.INTEGER,      
+        bairros_id:DataType.INTEGER,
+      },
+      {
+        tableName: "prestadores",
+        timestamps: false,
+      }
+    );
+  
+    Prestador.associate = (pastaModels) => {
+      Prestador.belongsTo(pastaModels.Zona, {
+        foreignKey: "zonas_id"
+      });
+  
+      Prestador.belongsTo(pastaModels.Cidade, {
+        foreignKey: "cidades_id"
+      });
+
+      Prestador.belongsTo(pastaModels.Bairro, {
+        foreignKey: "bairros_id"
+      });
+
+      //RELACIONAMENTO DE ZONA COM PRESTADOR M x N.
+      Prestador.belongsToMany(pastaModels.Zona,{
+        through: 'PrestadorZona',
+        foreignKey: 'prestadores_id',
+      });
+    };
+  
+    return Prestador;
+  };
+  
