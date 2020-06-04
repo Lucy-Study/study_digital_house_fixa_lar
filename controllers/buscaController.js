8c59970e78889db46916867ed7f76b389aabb0aa
Lucy <lucy.rodriguez.fontana@gmail.com>
2020-06-04 00:03:29 -0300

    página de busca sendo preparada para busca dinâmica

--- controllers/buscaController.js
+++ controllers/buscaController.js
@@ -1,9 +1,36 @@
 const Busca = require("../models/Busca");
 
+//ARRAY DE TESTE TEMP
+const listaTemp = [
+  { nome: "John Doe", regiao: "Leste", servicos: "Manutenção" },
+  { nome: "Airton", regiao: "Norte", servicos: "Conserto" },
+  { nome: "Pereira", regiao: "Sul", servicos: "Troca" },
+  { nome: "Zé", regiao: "Norte", servicos: "Conserto, Manutenção" },
+  { nome: "Everton", regiao: "Centro", servicos: "Manutenção, Troca" },
+  { nome: "John Doe", regiao: "Leste", servicos: "Manutenção" },
+  { nome: "Airton", regiao: "Norte", servicos: "Conserto" },
+  { nome: "Pereira", regiao: "Sul", servicos: "Troca" },
+  { nome: "Zé", regiao: "Norte", servicos: "Conserto, Manutenção" },
+  { nome: "Everton", regiao: "Centro", servicos: "Manutenção, Troca" },
+];
+
 let buscaController = {
-  buscarPrestador: (req, res) => {
-    let relacaoPrestadores = Busca.buscarPrestador();
+  listarPrestador: (req, res) => {
+    let relacaoPrestadores = Busca.listarPrestador();
     res.render("busca", { relacaoPrestadores });
+  },
+
+  // buscarPrestador: (req, res) => {
+  //   let resultadoPrestadores = Busca.buscarPrestador();
+  //   res.render("busca", { resultadoPrestadores });
+  //   console.log(typeof resultadoPrestadores);
+  // },
+  // };
+
+  //TESTE TEMP
+  buscarPrestador: (req, res) => {
+    let resultadoPrestadores = listaTemp;
+    res.render("busca", { resultadoPrestadores });
   },
 };
 
--- models/Busca.js
+++ models/Busca.js
@@ -1,9 +1,16 @@
-let prestador = [
-  { nome: "John Snow", regiao: "Winterfell", servicos: "Manutenção, Troca" },
-  { nome: "Arya Stark", regiao: "Winterfell", servicos: "Conserto, Troca" },
-  { nome: "Tyrion Lannister", regiao: "Kingslanding", servicos: "Reparo, Manutenção, Troca" },
-];
+const fs = require("fs");
+const path = require("path");
 
-function buscarPrestador() {
-  return prestador;
+const arquivoPrestador = path.join("tempArquivoPrestador.json");
+
+function listarPrestador() {
+  let listaDePrestadores = JSON.parse(fs.readFileSync(arquivoPrestador, { encoding: "utf-8" }));
+  return listaDePrestadores;
 }
+
+function buscarPrestador(posicao) {
+  let listaDePrestadores = JSON.parse(fs.readFileSync(arquivoPrestador, { encoding: "utf-8" }));
+  return listaDePrestadores[posicao];
+}
+
+module.exports = { listarPrestador, buscarPrestador };
