d1965f947a15a1435f3551b3733ca0de931fe175
Davi Jose <davijose40@gmail.com>
2020-06-18 05:20:54 -0300

    Updata config database.js
    usar variáveis .env e implementar o host, username, password que estiver implementado em cada banco local.

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
-  host: "localhost",
-  dialect: "mysql",
-  username: "davijose",
-  password: "davijose",
-  database: "fixadb",
+  host: process.env.HOST,
+  dialect: process.env.DB_PROVIDER,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   define: {
     underscore: true,
   },
