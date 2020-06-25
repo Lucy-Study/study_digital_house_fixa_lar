e074029e579465e37834fabc4da59bec02b63f78
Dorivan Grangeiro Almeida <dorivan.almeida@gmail.com>
2020-06-25 00:58:43 -0300

    adicionando model de newsletter

---
+++ models/Newsletter.js
@@ -0,0 +1,24 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Newsletter = sequelize.define('Newsletter', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    ativo: {
+      allowNull: false,
+      type: DataTypes.BOOLEAN
+    }
+  },{
+    tableName: 'newsletter',
+    freezeTableName: true
+  });
+
+  return Newsletter;
+};
