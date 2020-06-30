a9cc51adaad7ccbabab6818787b6ba44ac159bcb
LucyRodrigues <lucy.rodriguez.fontana@gmail.com>
2020-06-29 23:00:59 -0300

    add middleware nos botões de contratar dos cards do carroussel na Home

--- routes/contratarRoute.js
+++ routes/contratarRoute.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var router = express.Router();
+const { authUser } = require("../middleware/authPermitions");
 
-router.get("/", function (req, res, next) {
+router.get("/", authUser, function (req, res, next) {
   res.render("contratar");
 });
 
